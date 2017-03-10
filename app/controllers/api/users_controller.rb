3e472a44874475a7610c717ac94cf6045745c497
Daniel <daniel.j.fletcher2@gmail.com>
2017-03-10 12:06:35 -0800

    Create correctly nested Users and Session controllers and corresponding views

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,28 @@
+class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save!
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    if logged_in?
+      render :show
+    else
+      render json: ["You can't view another user's page"], status: 401
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
